19b7af75305c2e21a56dc2b81d35c7b5a3234a07
Erika001 <montsedan65@gmail.com>
2019-09-24 17:37:08 -0500

    Codigo clase dia 24 sept

---
+++ mayor_de_3.py
@@ -0,0 +1,22 @@
+NUM1 = int(input("Dame el primer nùmero"))
+NUM2 = int(input("Dame otro nùmero"))
+NUM3 = int(input("Dame un ultimo nùmero"))
+if NUM1 != NUM2 != and NUM1 != NUM3 and b != NUM3:
+    if NUM1 > NUM2 and NUM1 > NUM3:
+        print(NUM1, "es mayor")
+    else:
+        if NUM2 > NUM1 and NUM2 > NUM3:
+            print(NUM2,"es el mayor")
+        else:
+            print(NUM3. "es el mayor")
+
+print("La misma pero simplificada:")
+if NUM1 != NUM2 and NUM1 != NUM3 and NUM2 != NUM3:
+    if NUM1>NUM2 and NUM1>NUM3:
+        print(NUM1, "es el mayor")
+    elif NUM2 >NUM1 and NUM2>NUM3:
+        print(NUM2, "es el mayor")
+    if:
+        print(NUM3, "es el mayor")
+
+
